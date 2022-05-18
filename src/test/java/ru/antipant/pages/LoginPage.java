54ce59eeaf422b3368b8d82896cf7f908a263365
CORP\aantipov <Alexander.Antipov@comindware.com>
2022-05-18 10:36:38 +0300

    Add annotation to first test

--- src/test/java/ru/antipant/pages/LoginPage.java
+++ src/test/java/ru/antipant/pages/LoginPage.java
@@ -1,6 +1,7 @@
 package ru.antipant.pages;
 
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -10,6 +11,7 @@ public class LoginPage {
     SelenideElement password = $("#password");
     SelenideElement loginButton = $("#LoginButton");
 
+    @Step("Логинимся под {username} с паролем {pass}")
     public LoginPage login(String username, String pass) {
         open("");
         login.setValue(username);
--- src/test/java/ru/antipant/tests/LoginTests.java
+++ src/test/java/ru/antipant/tests/LoginTests.java
@@ -1,5 +1,6 @@
 package ru.antipant.tests;
 
+import io.qameta.allure.*;
 import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -7,8 +8,11 @@ import ru.antipant.config.CredentialsConfig;
 import ru.antipant.pages.BaseTest;
 import ru.antipant.pages.LoginPage;
 
-import static com.codeborne.selenide.Selenide.sleep;
 
+@Owner("Aantipov")
+@Severity(SeverityLevel.BLOCKER)
+@Feature("Страница логина")
+@Story("Страница логина и регистрации")
 public class LoginTests extends BaseTest {
     LoginPage loginPage = new LoginPage();
     CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
@@ -16,10 +20,8 @@ public class LoginTests extends BaseTest {
     String password = config.password();
 
     @Test
-    @DisplayName("Хороший тест")
+    @DisplayName("Залогиниться в платформу")
     void setLoginPage() {
         loginPage.login(login,password);
-        sleep(10000);
-
     }
 }
